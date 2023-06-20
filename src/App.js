f871c3c177c7bb3386ea1c169f66c31a6a03a9e7
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-06-20 18:53:44 -0400

    done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
+import { NavLink, Redirect } from 'react-router-dom'
 
 function App() {
   return (
     <div className='main'>
       <h1>App Component</h1>
+      <nav className="comp nav">
+        <ul>
+          <li>
+            {/* refreshes whole page :( */}
+            <a href="/movies">
+              anchor bad :(
+            </a>
+          </li>
+          <li>
+            <NavLink activeClassName="purple" activeStyle={{ fontWeight: 'bold' }} exact to="/" >Home</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="purple" activeStyle={{ fontWeight: 'bold' }} exact to="/movies" >Movies</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="purple" activeStyle={{ fontWeight: 'bold' }} exact to="/stocks" >Stocks</NavLink>
+          </li>
+        </ul>
+      </nav>
       <Switch>
         <Route exact path='/'>
           <Home />
@@ -16,6 +36,9 @@ function App() {
         </Route>
         <Route path='/movies'>
           <Movies />
+        </Route>
+        <Route path='/not-logged-in'>
+          <h1> You Must Be Logged In To Enter.</h1>
         </Route>
         <Route>
           <h1>Page Not Found</h1>
--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,7 +1,24 @@
+import { Redirect, useHistory } from 'react-router-dom'
+
 function Stocks() {
+  let history = useHistory()
+  console.log(history)
+
+  // this would be more like a cookie grab
+  let loggedIn = true;
+
+  if (!loggedIn) {
+    return <Redirect to="/not-logged-in"></Redirect>
+  }
+  const handleClick = () => {
+    window.alert('Sending info to the DB!');
+    history.push("/")
+  };
+
   return (
     <div className='comp orange'>
       <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
